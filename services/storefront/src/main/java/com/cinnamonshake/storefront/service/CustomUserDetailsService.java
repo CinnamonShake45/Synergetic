e0f7ef5d9dc650ba915e1aa7ff72c69300e29e7a
cinnamonshake45 <shikhardixit09@gmail.com>
2026-04-12 20:31:09 +0530

    cleared useless import

--- services/storefront/src/main/java/com/cinnamonshake/storefront/service/CustomUserDetailsService.java
+++ services/storefront/src/main/java/com/cinnamonshake/storefront/service/CustomUserDetailsService.java
@@ -4,7 +4,6 @@ import com.cinnamonshake.storefront.entity.User;
 import com.cinnamonshake.storefront.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
